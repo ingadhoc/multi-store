349ceff1c418f3f09fd69340b5dd3162ab9d1ef4
ValentinoDefelice <vd@adhoc.com.ar>
2018-08-14 09:39:32 -0300

    [REL] base_multi_store to V11. (#8)
    [FIX] View tags.
    [FIX] Update py files.
    [FIX] Update README.

--- base_multi_store/__manifest__.py
+++ base_multi_store/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Multi Stores Management',
-    'version': '9.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
@@ -42,7 +42,7 @@
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': True,
 }
--- base_multi_store/models/res_store.py
+++ base_multi_store/models/res_store.py
@@ -2,35 +2,38 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields, SUPERUSER_ID
-from odoo.osv import osv
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
-class res_store(models.Model):
+class ResStore(models.Model):
     _name = "res.store"
     _description = 'Stores'
     _order = 'parent_id desc, name'
 
     name = fields.Char(
-        'Name',
         required=True,
     )
+
     parent_id = fields.Many2one(
         'res.store',
         'Parent Store',
         index=True,
     )
+
     child_ids = fields.One2many(
         'res.store',
         'parent_id',
         'Child Stores'
     )
+
     company_id = fields.Many2one(
         'res.company', 'Company',
         # required=True,
         help='If specified, this store will be only available on selected '
         'company',
     )
+
     user_ids = fields.Many2many(
         'res.users',
         'res_store_users_rel',
@@ -43,27 +46,26 @@ class res_store(models.Model):
             'The store name must be unique per company!')
     ]
 
-    _constraints = [
-        (osv.osv._check_recursion,
-         'Error! You can not create recursive stores.', ['parent_id'])
-    ]
+    @api.constrains('parent_id')
+    def _check_parent_id(self):
+        for rec in self:
+            if not rec._check_recursion():
+                raise ValidationError(
+                    _('Error! You can not create recursive stores.'))
 
-    # Copy from res_company
+    @api.model
     def name_search(
-            self, cr, uid, name='', args=None, operator='ilike',
-            context=None, limit=100):
-        context = dict(context or {})
+            self, name='', args=None, operator='ilike', limit=100):
+        context = dict(self._context or {})
         if context.pop('user_preference', None):
             # We browse as superuser. Otherwise, the user would be able to
             # select only the currently visible stores (according to rules,
             # which are probably to allow to see the child stores) even if
             # she belongs to some other stores.
-            user = self.pool.get('res.users').browse(
-                cr, SUPERUSER_ID, uid, context=context)
+            self = self.sudo()
+            user = self.env.user
             store_ids = list(set(
                 [user.store_id.id] + [cmp.id for cmp in user.store_ids]))
-            uid = SUPERUSER_ID
             args = (args or []) + [('id', 'in', store_ids)]
-        return super(res_store, self).name_search(
-            cr, uid, name=name, args=args, operator=operator,
-            context=context, limit=limit)
+        return super(ResStore, self).name_search(
+            name=name, args=args, operator=operator, limit=limit)
