7f230204e49979ac94c84e7189f0dd14afccd08c
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-11-21 21:36:14 -0300

    [11.0] [IMP] to multi-store modules (#24)
    * [FIX] base_multi_store: Fix the name search method
    
    Readapt the method name_search  in res.store to filter only by the store that the user has allowed.
    Change in res user view to add in the domain to store witout company set.
    
    * [REF] account_multi_store: change the logic to restrict read accion
    
    * [REF] stock_multi_store: Readpt the logic to restring stores in stock

--- stock_multi_store/models/stock_warehouse.py
+++ stock_multi_store/models/stock_warehouse.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class StockWarehouse(models.Model):
@@ -15,3 +15,17 @@ class StockWarehouse(models.Model):
         " store, can only see this warehouse records but can not post or"
         " modify any record related to them."
     )
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        """
+        Para que usuarios los usuarios no puedan elegir almacenes donde no puedan
+        escribir, modificamos la funcion search. No lo hacemos por regla de
+        permiso ya que si no pueden ver los diarios termina dando errores en
+        cualquier lugar que se use un campo related a algo del diario
+        """
+        user = self.env.user
+        # if superadmin, do not apply
+        if user.id != 1:
+            args += ['|', ('store_id', '=', False), ('store_id', 'child_of', [user.store_id.id])]
+        return super().search(args, offset, limit, order, count=count)
