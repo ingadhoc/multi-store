208aa3904c3f92d8377f87d15a2ccac87d6a820c
Virginia Bonservizi <vib@adhoc.com.ar>
2022-08-16 13:09:42 +0000

    [MIG] purchase_multi_store: Migration to 15.0
    closes ingadhoc/multi-store#43
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- purchase_multi_store/__manifest__.py
+++ purchase_multi_store/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Multi Store for Purchase',
-    'version': '13.0.1.0.0',
+    'version': "15.0.1.0.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
@@ -38,7 +38,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
