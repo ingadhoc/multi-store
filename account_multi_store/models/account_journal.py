144802e9197e01fa6077620a86c24706d6f6d535
Virginia Bonservizi <vib@adhoc.com.ar>
2022-09-01 18:57:47 +0000

    [MIG] account_multi_store: Migration to 15.0
    closes ingadhoc/multi-store#45
    
    X-original-commit: 4ebdcff169bb3228381ec8d098d64848e7dff580
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- account_multi_store/models/account_journal.py
+++ account_multi_store/models/account_journal.py
@@ -19,7 +19,7 @@ class AccountJournal(models.Model):
     @api.model
     def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
         """
-        Para que usuarios los usuarios no puedan elegir diarios donde no puedan
+        Para que los usuarios no puedan elegir diarios donde no puedan
         escribir, modificamos la funcion search. No lo hacemos por regla de
         permiso ya que si no pueden ver los diarios termina dando errores en
         cualquier lugar que se use un campo related a algo del diario
