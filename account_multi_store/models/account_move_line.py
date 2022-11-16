de71f3b26b69676a990a1ef46fc6bcf11b421a3e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-11-16 11:56:30 +0000

    [IMP] account_ms: add option to force reconcile only on same store
    Part-of: ingadhoc/multi-store#48

--- account_multi_store/models/account_move_line.py
+++ account_multi_store/models/account_move_line.py
@@ -2,7 +2,8 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields
+from odoo import models, fields, _
+from odoo.exceptions import UserError
 
 
 class AccountMoveLine(models.Model):
@@ -12,3 +13,13 @@ class AccountMoveLine(models.Model):
         related='move_id.store_id',
         store=True,
     )
+
+    def reconcile(self):
+        store = None
+        for line in self:
+            if store is None:
+                store = line.store_id
+            elif line.store_id.only_allow_reonciliaton_of_this_store and store != line.store_id:
+                raise UserError(_(
+                    'For store "%s" you can only reconcile lines of same store') % (line.store_id.display_name))
+        return super().reconcile()
