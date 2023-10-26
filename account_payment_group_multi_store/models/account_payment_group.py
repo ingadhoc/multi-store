a62d6e89f492fb172b00c24a43f58c69ae40c576
Juan José Scarafia <jjs@adhoc.com.ar>
2023-10-26 01:22:25 +0000

    [FIX] account_payment_group_multi_store: payment from invoice
    Anteriormente estabamos computando el campo store_id con _compute_store_id pero eso nos traia cierta recursividad porque en esta version el campo _compute_to_pay_move_lines es comutado.
    
    Entonces lo que papsaba al pagar desde una factura era que previsuualizaba bien la deuda seleccionada pero cuando ibas a gregar una línea recomputaba deuda porque en realidad estaba recomputando el store (aunque en realidad no se estaba cambiando).
    
    Este no es el fix mas hermoso pero tmb es verdad que en teoría vamos a deprecar esto en 17
    
    Otra alternativa podría ser al revez, dejar computado el store_id y cambiar _compute_to_pay_move_lines y llamarlo por un onchange (pero no funciono a primera)
    
    Part-of: ingadhoc/multi-store#54

--- account_payment_group_multi_store/models/account_payment_group.py
+++ account_payment_group_multi_store/models/account_payment_group.py
@@ -12,13 +12,22 @@ class AccountPaymentGroup(models.Model):
         # default=lambda self: self.env.user.store_id,
     )
 
-    @api.depends('payment_ids')
-    def _compute_store_id(self):
-        # tal vez en este caso buscar un store padre que de alguna manera da
-        # permiso para todos estos stores?
-        for rec in self.filtered(lambda x: not x.store_id):
+    @api.model_create_multi
+    def create(self, vals_list):
+        """ Heredamos el create por si algun codigo de ecommerce o similra crea payemtns y payments groups
+        sin store y para intentar dejarlo asignado.
+        Tal vez en vez de ser en el create podria ser en un action_post?
+        Anteriormente estabamos conviriendo store_id a computado almacenado, pero había una suerte de recursividad
+        que nos traia problemas al menos en el caso en el cual veníamos desde pagar una factura (se mostraba bien
+        pero al querer agregar un pago nos recalculaba toda la deuda)
+        La realidad es que si esto nos trae problemas podríamos ignorar y ni siquiera computar este dato"""
+        recs = super().create(vals_list)
+        for rec in recs.filtered(lambda x: not x.store_id):
             if len(rec.payment_ids.mapped('journal_id.store_id')) == 1:
-                rec.store_id = rec.payment_ids.mapped('journal_id.store_id')
+                rec.store_id = rec.payment_ids.mapped('journal_id.store_id').id
+            elif len(rec.to_pay_move_line_ids.mapped('journal_id.store_id')) == 1:
+                rec.store_id = rec.to_pay_move_line_ids.mapped('journal_id.store_id').id
+        return recs
 
     def _get_to_pay_move_lines_domain(self):
         """ Si soy pago de un payment group con store, y :
@@ -33,6 +42,8 @@ class AccountPaymentGroup(models.Model):
 
     @api.depends('store_id')
     def _compute_to_pay_move_lines(self):
+        if self._context.get('default_to_pay_move_line_ids'):
+            return
         super()._compute_to_pay_move_lines()
 
     def compute_withholdings(self):
