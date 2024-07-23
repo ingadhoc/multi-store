5d6ea144ae106352533a76157451cd244a690ba3
Felipe Garcia Suez <feg@adhoc.com.ar>
2024-07-23 15:35:47 +0000

    [MIG] stock_multi_store: Migration to 17.0
    Part-of: ingadhoc/multi-store#56

--- stock_multi_store/__manifest__.py
+++ stock_multi_store/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Multi Store for Warehouse',
-    'version': "16.0.1.0.0",
+    'version': "17.0.1.0.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
@@ -44,7 +44,7 @@
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
