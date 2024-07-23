8f5cbc15718bfb8d7e2dfc5c41cf34b8f51fc1d1
Felipe Garcia Suez <feg@adhoc.com.ar>
2024-07-23 14:37:16 -0300

    [MIG] sale_multi_store: Migration to 17.0

--- sale_multi_store/__manifest__.py
+++ sale_multi_store/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Multi Store for Sale',
-    'version': "16.0.1.0.0",
+    'version': "17.0.1.0.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
@@ -38,7 +38,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
