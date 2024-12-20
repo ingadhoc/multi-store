5a4542108e71ceea5d0e9579a81932a1fbca1298
Felipe Garcia Suez <feg@adhoc.com.ar>
2024-12-20 15:55:45 -0300

    [FIX] multi_store without payment_pro

---
+++ account_multi_store/models/account_payment_register.py
@@ -0,0 +1,17 @@
+from odoo import models, _
+
+
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = "account.payment.register"
+
+    def _compute_available_journal_ids(self):
+        super()._compute_available_journal_ids()
+        restrict_store = self.env['res.store'].browse(self._context.get('restrict_store_id'))
+        if restrict_store:
+            for rec in self:
+                if restrict_store.only_allow_reonciliaton_of_this_store:
+                    rec.available_journal_ids = rec.available_journal_ids.filtered(
+                        lambda x: x.store_id == restrict_store)
+                elif restrict_store:
+                    rec.available_journal_ids = rec.available_journal_ids.filtered(
+                        lambda x: not x.store_id.only_allow_reonciliaton_of_this_store)
