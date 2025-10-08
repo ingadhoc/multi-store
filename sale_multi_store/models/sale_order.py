e2a5fbe041cd2d47038f9a5bf7e03f4fc188c540
Rocío Vega <rov@adhoc.com.ar>
2025-10-08 14:12:52 -0300

    [FIX] sale_multi_store: Fix compatibility with multi store and sale
    closes ingadhoc/multi-store#85
    
    Signed-off-by: Camila Vives <cav@adhoc.inc>

--- sale_multi_store/models/sale_order.py
+++ sale_multi_store/models/sale_order.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
@@ -12,3 +12,17 @@ class SaleOrder(models.Model):
         related="warehouse_id.store_id",
         store=True,
     )
+
+    @api.model
+    def _search(self, args, offset=0, limit=None, order=None):
+        """
+        Para que los usuarios no puedan elegir pickings donde no puedan
+        escribir, modificamos la funcion search. No lo hacemos por regla de
+        permiso ya que si no pueden ver los diarios termina dando errores en
+        cualquier lugar que se use un campo related a algo del diario
+        """
+        user = self.env.user
+        # if superadmin, do not apply
+        if not self.env.is_superuser():
+            args += ["|", ("store_id", "=", False), ("store_id", "child_of", [user.store_id.id])]
+        return super()._search(args, offset, limit, order)
