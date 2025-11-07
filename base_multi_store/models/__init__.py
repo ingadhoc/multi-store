8c400733cf2800a8f870dd7c6b85cec8849c6426
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-11-07 19:49:22 +0000

    [MIG] base_multi_store: Migration to 19.0

--- base_multi_store/models/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import res_store
-from . import res_users
